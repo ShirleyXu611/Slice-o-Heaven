fd233d0018ca4983e0baad76a5ac1c07271e7d93
ShirleyXu611 <shirley20051102@163.com>
2025-02-26 15:35:27 +0800

    exercise4

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args)throws Exception{
     Slice_o_Heaven pizza = new Slice_o_Heaven();
-    pizza.takeOrder("aaa",25.00);
+    pizza.takeOrder(); 
     }
 }
--- src/Slice_o_Heaven.java
+++ src/Slice_o_Heaven.java
@@ -10,14 +10,44 @@ public class Slice_o_Heaven {
     public String drinks;
     private String orderID;
     private double orderTotal;
+    public final String DEF_ORDER_ID = "DEF-SOH-099";
+    public final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
+    public final double DEF_ORDER_TOTAL = 15.00;
 
-    public void takeOrder(String id,Double total){
-        orderID=id;
-        orderTotal=total;
+    public Slice_o_Heaven(){
+        this.orderID = DEF_ORDER_ID;
+        this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
+        this.orderTotal = DEF_ORDER_TOTAL;
+        this.sides = 0;
+        this.drinks = "";
+    }
+
+    public Slice_o_Heaven(String orderID,String pizzaIngredients,double orderTotal,int sides,String drinks){
+        this.orderID = DEF_ORDER_ID;
+        this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
+        this.orderTotal = DEF_ORDER_TOTAL;
+        this.sides = 0;
+        this.drinks = "";
+    }
+
+    public String getOrderID(){
+        return orderID;
+    }
+    public void setOrderID(String orderID){
+        this.orderID=orderID;
+    }
+    public double getOrderTotal(){
+        return orderTotal;
+    }
+    public void setOrderTotal(double orderTotal){
+        this.orderTotal=orderTotal;
+    }
+
+    public void takeOrder() {
         System.out.println("Order accepted!");
-        try{
+        try {
             Thread.sleep(5000);
-        } catch (InterruptedException e){
+        } catch (InterruptedException e) {
             makePizza();
             System.out.println("Order is ready for pickup!");
         }
@@ -25,18 +55,21 @@ public class Slice_o_Heaven {
         System.out.println("Order is ready for pickup!");
         printRecipt();
     }
-    public void makePizza(){
+
+    private void makePizza() {
         System.out.println("Pizza start making!");
-        try{
+        try {
             Thread.sleep(5000);
-        } catch (InterruptedException e){
+        } catch (InterruptedException e) {
             System.out.println("The pizza is finished!");
         }
         System.out.println("The pizza is finished!");
     }
-    public void printRecipt(){
+
+    private void printRecipt() {
         System.out.println("********RECEIPT********");
-        System.out.println("Order ID:"+orderID);
-        System.out.println("Order Total:"+orderTotal);
+        System.out.println("Order ID:" + orderID);
+        System.out.println("Pizza Ingredients:"+pizzaIngredients);
+        System.out.println("Order Total:" + orderTotal);
     }
 }
