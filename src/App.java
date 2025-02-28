b3d1b8f5d588e183cbd9978ab677553f8859e69a
ShirleyXu611 <shirley20051102@163.com>
2025-02-28 14:09:11 +0800

    exercise5

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args)throws Exception{
     Slice_o_Heaven pizza = new Slice_o_Heaven();
+    pizza.specialOfTheDay("cheese pizza", "chips", "$9.99");
     pizza.takeOrder(); 
+    pizza.processCardPayment("12345678912345","02.11.2025", 345);
     }
 }
--- src/Slice_o_Heaven.java
+++ src/Slice_o_Heaven.java
@@ -6,27 +6,61 @@ public class Slice_o_Heaven {
     public String storeMenu;
     public String pizzaIngredients;
     public double pizzaPrice;
-    public int sides;
+    public String sides;
     public String drinks;
     private String orderID;
     private double orderTotal;
     public final String DEF_ORDER_ID = "DEF-SOH-099";
     public final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
     public final double DEF_ORDER_TOTAL = 15.00;
+    private String cardNumber;
+    private int cardLength;
+    private int firstCardDigit;
+    private String blacklistedNumber;
+    private int lastFourDigits;
+    public String cardNumberToDisplay;
+
+    public void processCardPayment(String cardNumber,String expiryDate,int cvv){
+        this.cardNumber=cardNumber;
+        cardLength=cardNumber.length();
+        if(cardLength==14){
+            System.out.println("Card accepted.");
+        }else{
+            System.out.println("Invalid cards.");
+        }
+        String cN1=cardNumber.substring(0,1);
+        firstCardDigit=Integer.parseInt(cN1);
+        if(cardNumber.equals(blacklistedNumber)){
+            System.out.println("Card is blacklisted.Please use another card.");
+        
+        }
+        String cN2=cardNumber.substring(10,14);
+        lastFourDigits=Integer.parseInt(cN2);
+        StringBuilder sb = new StringBuilder(cardNumber);
+        sb.replace(1, 10, "*********");
+        String cardNumberToDisplay = sb.toString(); 
+        System.out.println(cardNumberToDisplay);
+    }
+
+    public void specialOfTheDay(String pizzaOfTheDay,String sidesOfTheDay,String specialPrice){
+        System.out.println("The special pizza of the day is:"+pizzaOfTheDay);
+        System.out.println("The special sides of the day is:"+sidesOfTheDay);
+        System.out.println("The special price of the day is:"+specialPrice);
+    }
 
     public Slice_o_Heaven(){
         this.orderID = DEF_ORDER_ID;
         this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
         this.orderTotal = DEF_ORDER_TOTAL;
-        this.sides = 0;
+        this.sides = "";
         this.drinks = "";
     }
 
-    public Slice_o_Heaven(String orderID,String pizzaIngredients,double orderTotal,int sides,String drinks){
+    public Slice_o_Heaven(String orderID,String pizzaIngredients,double orderTotal,String sides,String drinks){
         this.orderID = DEF_ORDER_ID;
         this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
         this.orderTotal = DEF_ORDER_TOTAL;
-        this.sides = 0;
+        this.sides = "";
         this.drinks = "";
     }
 
